e2043c3ccb9f4db7239eda521f90a600c84e7fed
maaza17 <maaz.haque17@gmail.com>
2022-12-24 18:16:09 +0500

    report model initial understanding

--- models/report.model.js
+++ models/report.model.js
@@ -1,6 +1,42 @@
 const mongoose = require('mongoose')
 
 // define report schema
-const reportSchema = new mongoose.Schema({})
+const reportSchema = new mongoose.Schema({
+    date: {
+        type: Date
+    },
+    comID: {
+        type: String,
+        required: true
+    },
+    imressions: {
+        type: Number,
+        required: true
+    },
+    clicks: {
+        type: Number,
+        required: true
+    },
+    adRequests: {
+        type: Number,
+        required: true
+    },
+    ctr: {
+        type: Number,
+        required: true
+    },
+    cpm: {
+        type: Number,
+        required: true
+    },
+    revenue: {
+        type: Number,
+        required: true
+    },
+    uploadDate: {
+        type: Date,
+        default: Date.now()
+    }
+})
 const reportModel = mongoose.model('report', reportSchema)
 module.exports = reportModel
