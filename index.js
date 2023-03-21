21c9ce27c6fbdad1f2f0f037bb28fc99c97011c3
haris316 <gogeto931@gmail.com>
2023-03-21 20:07:25 +0500

    Changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const userRoute = require('./routes/user.route')
 const app = express();
 
 var cors = require('cors');
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
 
 app.use(bodyParser.json({ limit: 20000000 }));
 app.use(cookieParser())
