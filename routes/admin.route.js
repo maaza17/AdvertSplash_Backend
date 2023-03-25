0466b8f8e7b8d3e0db85b6326b1c17bea24f5777
haris316 <gogeto931@gmail.com>
2023-03-26 02:31:51 +0500

    cookie jugar

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -84,12 +84,10 @@ router.post('/login', async (req, res) => {
 // logout admin
 router.get('/logout', async (req, res) => {
     res.cookie('auth_token_adm', 'none', {
-        expires: new Date(Date.now() + 10),
         httpOnly: true,
-        secure: process.env.NODE_ENV == 'production',
         sameSite: "none"
     })
-    
+
     return res.status(200).json({
         message: "Logout successful! 4",
     })
